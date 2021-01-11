a9ac28d8e55497705d125611adfe57d0c3188479
khanhtaquoc98 <khanhtaquoc98@gmail.com>
2021-01-11 21:11:19 +0700

    u

--- app.py
+++ app.py
@@ -95,26 +95,7 @@ def no_marks(s):
     return result
 
 def normalize_text(text):
-    path_nag = 'sentiment_dicts/nag.txt'
-    path_pos = 'sentiment_dicts/pos.txt'
-    path_not = 'sentiment_dicts/not.txt'
-
-    VN_CHARS_LOWER = u'ạảãàáâậầấẩẫăắằặẳẵóòọõỏôộổỗồốơờớợởỡéèẻẹẽêếềệểễúùụủũưựữửừứíìịỉĩýỳỷỵỹđð'
-    VN_CHARS_UPPER = u'ẠẢÃÀÁÂẬẦẤẨẪĂẮẰẶẲẴÓÒỌÕỎÔỘỔỖỒỐƠỜỚỢỞỠÉÈẺẸẼÊẾỀỆỂỄÚÙỤỦŨƯỰỮỬỪỨÍÌỊỈĨÝỲỶỴỸÐĐ'
-    VN_CHARS = VN_CHARS_LOWER + VN_CHARS_UPPER
-
-    
-    with codecs.open(path_nag, 'r',  encoding="utf8") as f:
-        nag = f.readlines()
-    nag_list = [n.replace('\n', '') for n in nag]
-
-    with codecs.open(path_pos, 'r',  encoding="utf8") as f:
-        pos = f.readlines()
-    pos_list = [n.replace('\n', '') for n in pos]
-
-    with codecs.open(path_not, 'r',  encoding="utf8") as f:
-        not_ = f.readlines()
-    not_list = [n.replace('\n', '') for n in not_]
+   
 
     #Remove các ký tự kéo dài: vd: đẹppppppp
     text = re.sub(r'([A-Z])\1+', lambda m: m.group(1).upper(), text, flags=re.IGNORECASE)
@@ -223,27 +204,6 @@ def normalize_text(text):
     return text
 
 def TrainDataSentiment():
-    path_nag = 'sentiment_dicts/nag.txt'
-    path_pos = 'sentiment_dicts/pos.txt'
-    path_not = 'sentiment_dicts/not.txt'
-
-    VN_CHARS_LOWER = u'ạảãàáâậầấẩẫăắằặẳẵóòọõỏôộổỗồốơờớợởỡéèẻẹẽêếềệểễúùụủũưựữửừứíìịỉĩýỳỷỵỹđð'
-    VN_CHARS_UPPER = u'ẠẢÃÀÁÂẬẦẤẨẪĂẮẰẶẲẴÓÒỌÕỎÔỘỔỖỒỐƠỜỚỢỞỠÉÈẺẸẼÊẾỀỆỂỄÚÙỤỦŨƯỰỮỬỪỨÍÌỊỈĨÝỲỶỴỸÐĐ'
-    VN_CHARS = VN_CHARS_LOWER + VN_CHARS_UPPER
-
-    
-    with codecs.open(path_nag, 'r',  encoding="utf8") as f:
-        nag = f.readlines()
-    nag_list = [n.replace('\n', '') for n in nag]
-
-    with codecs.open(path_pos, 'r',  encoding="utf8") as f:
-        pos = f.readlines()
-    pos_list = [n.replace('\n', '') for n in pos]
-
-    with codecs.open(path_not, 'r',  encoding="utf8") as f:
-        not_ = f.readlines()
-    not_list = [n.replace('\n', '') for n in not_]
-
     ds = DataSource()
     train_data = pd.DataFrame(ds.load_data('data_clean/train.crash'))
     new_data = []
@@ -311,6 +271,27 @@ def TrainDataSentiment():
 
 @app.route('/')
 def index():
+    path_nag = 'sentiment_dicts/nag.txt'
+    path_pos = 'sentiment_dicts/pos.txt'
+    path_not = 'sentiment_dicts/not.txt'
+
+    VN_CHARS_LOWER = u'ạảãàáâậầấẩẫăắằặẳẵóòọõỏôộổỗồốơờớợởỡéèẻẹẽêếềệểễúùụủũưựữửừứíìịỉĩýỳỷỵỹđð'
+    VN_CHARS_UPPER = u'ẠẢÃÀÁÂẬẦẤẨẪĂẮẰẶẲẴÓÒỌÕỎÔỘỔỖỒỐƠỜỚỢỞỠÉÈẺẸẼÊẾỀỆỂỄÚÙỤỦŨƯỰỮỬỪỨÍÌỊỈĨÝỲỶỴỸÐĐ'
+    VN_CHARS = VN_CHARS_LOWER + VN_CHARS_UPPER
+
+    
+    with codecs.open(path_nag, 'r',  encoding="utf8") as f:
+        nag = f.readlines()
+    nag_list = [n.replace('\n', '') for n in nag]
+
+    with codecs.open(path_pos, 'r',  encoding="utf8") as f:
+        pos = f.readlines()
+    pos_list = [n.replace('\n', '') for n in pos]
+
+    with codecs.open(path_not, 'r',  encoding="utf8") as f:
+        not_ = f.readlines()
+    not_list = [n.replace('\n', '') for n in not_]
+    clfSentiment = TrainDataSentiment()
      return "<h1>Connect</h1>"
 
 @app.route('/sentcomment', methods=['POST'])
@@ -333,7 +314,7 @@ def sentcomment():
        
 if __name__ == '__main__':
     app.debug = True
-    clfSentiment = TrainDataSentiment()
+   
     app.run()
     
  
